63ddbc9af8686995febf8a0325e7324b4e052f12
Rajesh Yadav <rajeshyadav02724@gmail.com>
2026-07-08 00:16:39 +0530

    type casting of python done

--- 1-variable/var_6_casting.py
+++ 1-variable/var_6_casting.py
@@ -24,5 +24,61 @@ When using bool(), only 0, 0.0, empty strings "", empty lists [], and None conve
 a = 12
 b = 10.5
 c = a+b
-print(c) #22.5
+print(type(c),c) #22.5
+
+age = 23
+int_to_str = str(age)
+print(type(int_to_str),int_to_str)
+# "23"
+
+int_to_float = float(age)
+print(type(int_to_float),int_to_float) #23.0
+
+name = "Rajesh"
+# we must use base 10 means number
+#int_name = int(name) 
+
+num_str = "12"
+str_to_int = int(num_str)
+print(type(str_to_int),str_to_int)#12
+
+str_to_float = float(num_str)
+print(type(str_to_float),str_to_float)
+#12.0
+
+float_num = 23.6
+float_to_int = int(float_num)
+print(type(float_to_int),float_to_int)
+#23
+float_to_str = str(float_num)
+print(type(float_to_str),float_to_str)
+# "23.6"
+
+print(type(bool(0)),bool(0))#False
+print(type(bool(1)),bool(1))#True
+
+print(type(bool(" ")),bool(" "))#True
+
+print(type(bool("")),bool(""))#False
+
+print(type(bool({"id":1})),bool({"id":1}))#True
+print(type(bool({})),bool({}))#False
+
+print(type(bool([])),bool([]))#False
+print(type(bool([1])),bool([1]))#True
+
+print(type(bool(())),bool(()))#False
+print(type(bool((1,))),bool((1,)))
+#True
+
+print(type(bool(0.0)),bool((0.0)))
+#False
+print(type(bool(0.2)),bool((0.2)))
+#True
+
+print(type(bool(None)),bool((None)))
+#False
+
+
+
 
