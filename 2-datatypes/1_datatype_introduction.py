9f1150d276231a240d1d66c5f26d542643880496
Rajesh Yadav <rajeshyadav02724@gmail.com>
2026-07-08 20:30:28 +0530

    adding new chapter data type

---
+++ 2-datatypes/1_datatype_introduction.py
@@ -0,0 +1,35 @@
+"""
+Python data types are actually classes, and the defined variables are their instances or objects. Since Python is dynamically typed, the data type of a variable is determined at runtime based on the assigned value.
+
+In general, the data types are used to define the type of a variable. It represents the type of data we are going to store in a variable and determines what operations can be done on it.
+"""
+
+"""
+Number Data Type:
+int 
+float 
+complex 
+boolean 
+
+Set Data Type:
+set 
+frozenset
+
+Sequence Data Type:
+list 
+tuple 
+range 
+string 
+
+Binary Data Type:
+bytes
+bytearray 
+memoryview
+
+Dictionary Data Type
+
+Boolean Data Type
+
+None Data Type
+"""
+
