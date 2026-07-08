7f2ecd326afca20ad1ce3c4d3136ea75fef6578c
Rajesh Yadav <rajeshyadav02724@gmail.com>
2026-07-08 14:02:10 +0530

    adding constant files

---
+++ 1-variable/11_variable_constant.py
@@ -0,0 +1,8 @@
+"""
+Python doesn't have any formally defined constants, However you can indicate a variable to be treated as a constant by using all-caps names with underscores. For example, the name PI_VALUE indicates that you don't want the variable redefined or changed in any way.
+"""
+
+radius = 5
+PI_VALUE = 3.14
+area_of_circle = PI_VALUE*radius*radius
+print("Area of circle:",area_of_circle)
