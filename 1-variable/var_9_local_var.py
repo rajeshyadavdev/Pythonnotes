318e67894cf3777809e388320e3df4458b8a1d9f
Rajesh Yadav <rajeshyadav02724@gmail.com>
2026-07-08 11:37:59 +0530

    adding local variable concept

---
+++ 1-variable/var_9_local_var.py
@@ -0,0 +1,13 @@
+"""
+Python Local Variables are defined inside a function. We can not access variable outside the function.
+num1 and num2 are local Variables
+"""
+
+def add():
+  num1 = 20 
+  num2 = 30
+  print("Add of",num1,"and",num2,"=",num1+num2)
+
+
+
+add() 
